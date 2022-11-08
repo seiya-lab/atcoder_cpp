bab67b1840ec62f3e2b0d18880830abc803a8c46
seiya-lab <scb01088@edu.osakafu-u.ac.jp>
2022-11-08 09:45:21 +0900

    created environment for atcoder_cpp

---
+++ ddcc2020-qual/a/a.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int x,y;
+	cin>>x>>y;
+	long long ans = 0;
+	if(x == 1)
+	{
+		ans+=300000;
+	}
+	else if(x == 2)
+	{
+		ans+=200000;
+	}
+	else if(x == 3)
+	{
+		ans+=100000;
+	}
+	if(y == 1)
+	{
+		ans+=300000;
+	}
+	else if(y == 2)
+	{
+		ans+=200000;
+	}
+	else if(y == 3) 
+	{
+		ans+=100000;
+	}
+	if(x == 1&&y == 1)
+	{
+		ans+=400000;
+	}
+	cout<<ans<<endl;
+	return 0;
+}
